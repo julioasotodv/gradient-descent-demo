689df0c62be2a329a8634ce5718547db0b0b83bf
SOTOVJU1 <julio_antonio.soto_vicente@sandoz.com>
2020-12-03 13:26:38 +0100

    Cambiada estructura del proyecto

--- bokehserverextension.py
+++ bokehserverextension.py
@@ -3,4 +3,4 @@ from subprocess import Popen
 
 def load_jupyter_server_extension(nbapp):
     """serve the bokeh-app directory with bokeh server"""
-Popen(["panel", "serve", "gradient_descent.py", "--allow-websocket-origin=*"])
+Popen(["panel", "serve", "gradient_descent", "--allow-websocket-origin=*"])
