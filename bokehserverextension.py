87b05897a2324a37b65f405aaa03d0c4c1d5394c
Julio Antonio Soto <julio@MacBook-Pro.local>
2023-01-09 23:45:45 +0100

    Minor changes

--- bokehserverextension.py
+++
@@ -1,6 +0,0 @@
-from subprocess import Popen
-
-
-def load_jupyter_server_extension(nbapp):
-    """serve the bokeh-app directory with bokeh server"""
-    Popen(["panel", "serve", "gradient_descent", "--allow-websocket-origin=*"])
